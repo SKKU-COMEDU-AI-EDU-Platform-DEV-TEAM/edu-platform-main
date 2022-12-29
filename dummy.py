5f28bc748d810095b90776ea15a57b7923065204
ctwc55 <ssy990408@gmail.com>
2022-12-29 19:08:50 +0900

    create module for dummy data

---
+++ dummy.py
@@ -0,0 +1,86 @@
+courseJson = {
+    "data": [
+        { "id": 1, "name": "EDA", "size": 350, "week": 10 },
+        { "id": 2, "name": "API", "size": 100, "week": 10 },
+        { "id": 3, "name": "결측치", "size": 75, "week": 1 },
+        { "id": 4, "name": "이상치", "size": 150, "week": 1 },
+        { "id": 5, "name": "정량 데이터", "size": 150, "week": 2 },
+        { "id": 6, "name": "정성 데이터", "size": 125, "week": 5 },
+        { "id": 7, "name": "회귀", "size": 230, "week": 3 },
+        { "id": 8, "name": "군집화", "size": 70, "week": 3 },
+        { "id": 9, "name": "분류", "size": 70, "week": 15 },
+        { "id": 10, "name": "확률", "size": 70, "week": 4 },
+        { "id": 11, "name": "질적자료", "size": 70, "week": 4 },
+        { "id": 12, "name": "양적자료", "size": 50, "week": 6 },
+        { "id": 13, "name": "웹크롤링", "size": 60, "week": 13 },
+        { "id": 14, "name": "Python", "size": 70, "week": 5 },
+        { "id": 15, "name": "DIKW", "size": 80, "week": 7 },
+        { "id": 16, "name": "빅데이터", "size": 90, "week": 8 },
+        { "id": 17, "name": "numpy", "size": 100, "week": 12 },
+        { "id": 18, "name": "matplotlib", "size": 150, "week": 9 },
+        { "id": 19, "name": "Series", "size": 150, "week": 14 },
+        { "id": 20, "name": "DataFrame", "size": 150, "week": 11 },
+    ],
+    "metaverse":[
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta",
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta",
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta",
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta",
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta",
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta",
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta",
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta",
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta",
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta",
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta",
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta",
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta",
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta",
+        "https://app.gather.town/app/KxbGPczKS6ld3Fxt/SKKUMeta"
+    ]
+}
+
+quizJson = {
+    "data":[
+        {
+            "question": "다음 보기 중 정량 데이터 분석과 정성 데이터 분석에 알맞은 데이터로 제시되지 않은 것을 고르시오.",
+            "definition": "정량 데이터 분석이란 ‘수치 데이터’를 바탕으로 분석하는 방법으로, 사람의 경험이나 느낌에 좌우되지 않는다. 결과의 확실성을 위해 많은 샘플 데이터 수집을 필요로 한다. 정성 데이터 분석이란 숫자를 사용하지 않고 ‘질적 데이터’를 바탕으로 분석하는 방법을 말한다. 정량 데이터에 비해 객관성이 부족하다.",
+            "option": [
+                "정량 데이터 : 회사 매출 데이터, 정성 데이터 : 고객들의 구매 동기 데이터",
+                "정량 데이터 : 학생 성적 데이터, 정성 데이터 : 손글씨 사진 데이터",
+                "정량 데이터 : 평균 기온 데이터, 정성 데이터 : 버스 배차 간격 데이터",
+                "정량 데이터 : 미세먼지 농도 데이터, 정성 데이터 : 횡단보도 사진 데이터"
+            ]
+        },
+        {
+            "question": "다음 보기 중 독립 변수와 종속 변수가 알맞게 제시된 것으로 옳지 않은 것을 고르시오.",
+            "definition": "독립 변수란 종속 변수의 원인이 되는 변수다.",
+            "option": [
+                "독립 변수 : 마스크 착용 여부, 종속 변수 : 백신 접종 횟수",
+                "독립 변수 : 공부 집중 시간, 종속 변수 : 교과 성적",
+                "독립 변수 : 흡연 빈도, 종속 변수 : 폐암 발병률",
+                "독립 변수 : 운동 횟수, 종속 변수 : 근육량"
+            ]
+        },
+        {
+            "question": "다음 보기 중 데이터 분석 유형에 관한 설명으로 옳지 않은 것을 고르시오.",
+            "definition": "Descrptive는 서술적 혹은 기술적, inferential은 추론적, predictive는 예측적, mechanistic은 기계적이라는 뜻이다.",
+            "option": [
+                "Descriptive analysis : 주어진 데이터를 요약/집계하여 결과를 도출",
+                "Inferential analysis : 샘플-모집단 간의 관계를 탐구",
+                "Predictive analysis : 통계적 기법을 사용하여 미래, 발생하지 않은 사건에 대한 예측",
+                "Mechanistic analysis : 여러 변수 간 트렌드 또는 패턴 및 관계를 도출"
+            ]
+        },
+        {
+            "question": "다음 보기 중 군집화 분석으로 가장 적절한 데이터를 찾아 고르시오.",
+            "definition": "데이터 분석 방법에는 회귀, 분류, 군집화 등이 있다.",
+            "option": [
+                "학습 시간에 따른 집중력 변화 데이터",
+                "다양한 종의 강아지 사진 데이터",
+                "학습자 유형 별 성적 데이터",
+                "서울 아파트 면적에 따른 아파트 가격 데이터"
+            ]
+        }
+    ]
+}
