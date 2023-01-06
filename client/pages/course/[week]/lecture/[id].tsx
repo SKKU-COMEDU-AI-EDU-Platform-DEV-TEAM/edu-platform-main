d2eeed409ffa58e735a9983499184767150fa260
nimod7890 <nimod1234@g.skku.edu>
2023-01-06 20:50:17 +0900

    fe: update lecture page

--- client/pages/course/[week]/lecture/[id].tsx
+++ client/pages/course/[week]/lecture/[id].tsx
@@ -7,35 +7,39 @@ import { userState } from "../../../../recoil";
 import { Lecture, User } from "../../../../types";
 import { Box, Text, Stack, AspectRatio, StackDivider } from "@chakra-ui/react";
 import axios from "axios";
+import { useMutation } from "react-query";
 
 export default function LecturePage() {
   const router = useRouter();
   const { week, id } = router.query;
-  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [content, setContent] = useState<Lecture>();
   const user = useRecoilValue<User | null>(userState);
+  const lecture = async () => {
+    const { data } = await axios.post("/api/lecture/", {
+      week: week,
+      id: id,
+      token: user!.token
+    });
 
+    return data;
+  };
+  const { mutate } = useMutation(lecture, {
+    onSuccess: (data) => {
+      setContent(data.data);
+    },
+    onError: (err) => {
+      alert(err);
+    }
+  });
   useEffect(() => {
-    const fetchData = async () => {
-      if (!router.isReady) return;
-
-      const response = (
-        await axios.post(`/api/lecture/${week}/${id}`, {
-          token: user!.token
-        })
-      ).data;
-
-      setContent(response.data);
-      setIsLoading(false);
-    };
-    fetchData();
-  }, [router.isReady]);
+    mutate();
+  }, []);
 
   return (
     <Layout>
       <CourseLayout
         title={`${week}주차 | ${content?.title} `}
-        type={user!.type}
+        type={user?.type}
         metaverse={""}
       >
         <>
@@ -45,7 +49,7 @@ export default function LecturePage() {
                 {String(id).padStart(2, "0")}.
               </Text>
               <Text fontWeight={"bold"} textAlign={"left"} pl={2}>
-                {content!.videoTitle}
+                {content?.videoTitle}
               </Text>
             </Stack>
           </Box>
@@ -59,13 +63,13 @@ export default function LecturePage() {
           >
             <AspectRatio ratio={16 / 9}>
               <iframe
-                title={content!.title}
-                src={content!.video}
+                title={content?.title}
+                src={content?.video}
                 allowFullScreen
               />
             </AspectRatio>
             <AspectRatio ratio={16 / 9}>
-              <embed src={content!.pdf} type="application/pdf" />
+              <embed src={content?.pdf} type="application/pdf" />
             </AspectRatio>
           </Stack>
         </>
